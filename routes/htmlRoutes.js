f699fcd97922114eb34aaef5cfb968b6edcf6290
Aidan <84754533+aidan10371@users.noreply.github.com>
2022-10-15 15:36:02 -0400

    about

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -165,7 +165,7 @@ module.exports = function (app) {
 
   app.get("/html/about", (req, res) => {
     if (req.userPermissions > 2) {
-      fs.readFile(path.join(__dirname, "../history.txt"), 'utf8', function(err, data) {
+      fs.readFile(path.join(__dirname, "../about.txt"), 'utf8', function(err, data) {
         if (err) return res.status(500).end();
         dataRes(res, req, true, {about: data})
       });
